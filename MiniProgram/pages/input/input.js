fece8fba0421576b8e35a741fc4de9a7717abe6f
jinguangWang <wangjgcsdn@163.com>
2019-11-29 10:49:52 +0800

    常用组件

---
+++ MiniProgram/pages/input/input.js
@@ -0,0 +1,17 @@
+// pages/input/input.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  handleFocus(){
+    console.log(11111)
+  },
+  handleInput(){
+    console.log('input......')
+  }
+})
